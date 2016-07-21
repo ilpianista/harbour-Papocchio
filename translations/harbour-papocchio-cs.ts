a2d6012aab40e5adced0e38187aca49b84c12266
Andrea Scarpino <me@andreascarpino.it>
2016-07-21 20:37:00 +0200

    Restore Czech translation

--- translations/harbour-papocchio-cs.ts
+++ translations/harbour-papocchio-cs.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="cs">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.1">
 <context>
     <name>CoverPage</name>
     <message>
@@ -14,17 +12,17 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="76"/>
         <source>Saving the canvas...</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukládá se plátno...</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="112"/>
         <source>Clearing the canvas...</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyprazdňuje se plátno...</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="129"/>
         <source>Quitting...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
