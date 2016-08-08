03e25f66f7410f2bf6b72d5bf4d8349785381da8
Andrea Scarpino <me@andreascarpino.it>
2016-08-08 19:50:55 +0200

    Add Swedish translation

--- translations/harbour-papocchio-sv.ts
+++ translations/harbour-papocchio-sv.ts
@@ -1,10 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.1">
 <context>
     <name>CoverPage</name>
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="25"/>
         <source>Draw!</source>
-        <translation>Tekenen!</translation>
+        <translation>Rita!</translation>
     </message>
 </context>
 <context>
@@ -12,17 +12,17 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="76"/>
         <source>Saving the canvas...</source>
-        <translation>Opslaan canvas...</translation>
+        <translation>Sparar bilden...</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="112"/>
         <source>Clearing the canvas...</source>
-        <translation>Leegmaken canvas...</translation>
+        <translation>Rensar ytan...</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="129"/>
         <source>Quitting...</source>
-        <translation>Afsluiten...</translation>
+        <translation>Avslutar...</translation>
     </message>
 </context>
 </TS>
