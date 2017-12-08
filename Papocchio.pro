cd8ea5f1a7e53b172d0962f836e6ff5d330ad159
Andrea Scarpino <me@andreascarpino.it>
2017-12-08 21:19:05 +0100

    Update translations

--- Papocchio.pro
+++ Papocchio.pro
@@ -18,7 +18,12 @@ OTHER_FILES += \
 CONFIG += sailfishapp_i18n
 TRANSLATIONS += \
     translations/harbour-papocchio-cs.ts \
+    translations/harbour-papocchio-de.ts \
+    translations/harbour-papocchio-el.ts \
+    translations/harbour-papocchio-hu.ts \
     translations/harbour-papocchio-it.ts \
+    translations/harbour-papocchio-nb_NO.ts \
     translations/harbour-papocchio-nl.ts \
+    translations/harbour-papocchio-ru_RU.ts \
     translations/harbour-papocchio-sr.ts \
     translations/harbour-papocchio-sv.ts
---
+++ translations/harbour-papocchio-nb_NO.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="nb_NO" version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="25"/>
+        <source>Draw!</source>
+        <translation>Tegn!</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>Saving the canvas…</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="112"/>
+        <source>Clearing the canvas…</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="129"/>
+        <source>Quitting…</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
