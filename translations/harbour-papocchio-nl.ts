770b7615f76c6bd0c54b6c7bf663c119a54657c0
Heimen Stoffels <vistausss@outlook.com>
2018-02-20 16:59:53 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Harbour Papocchio/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-papocchio/translations/nl/

--- translations/harbour-papocchio-nl.ts
+++ translations/harbour-papocchio-nl.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="nl" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS language="nl" version="2.1">
 <context>
     <name>CoverPage</name>
     <message>
@@ -12,17 +14,17 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="76"/>
         <source>Saving the canvas…</source>
-        <translation>Canvas wordt opgeslagen…</translation>
+        <translation>Bezig met opslaan van canvas…</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="112"/>
         <source>Clearing the canvas…</source>
-        <translation>Canvas wordt leeggemaakt…</translation>
+        <translation>Bezig met wissen van canvas…</translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="129"/>
         <source>Quitting…</source>
-        <translation>Afsluiten…</translation>
+        <translation>Bezig met afsluiten…</translation>
     </message>
 </context>
 </TS>
