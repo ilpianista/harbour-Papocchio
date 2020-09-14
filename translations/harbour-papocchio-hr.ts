c7527b017a73ffdc6bc6001fb54f981856418576
Milo Ivir <mail@milotype.de>
2020-09-14 13:39:41 +0200

    Added translation using Weblate (Croatian)

---
+++ translations/harbour-papocchio-hr.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="25"/>
+        <source>Draw!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>Saving the canvas…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="112"/>
+        <source>Clearing the canvas…</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="129"/>
+        <source>Quitting…</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
